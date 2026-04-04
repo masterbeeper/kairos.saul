ec14ff6ec62c104545a9b5e1122efa7c777be02e
SawOo AungKhing <sawooaungkhing@SawOos-MacBook-Pro.local>
2026-04-04 17:59:47 +0800

    Update Email

--- assets/js/config.js
+++ assets/js/config.js
@@ -17,7 +17,7 @@ const KAIROS_CONFIG = {
   // Only these Google accounts can access the dashboard
   // Add as many emails as you need
   ALLOWED_EMAILS: [
-    'rw.htun@gmail.com',
+    'zhaolibin87@gmail.com',
     // 'family@gmail.com',
   ],
 
